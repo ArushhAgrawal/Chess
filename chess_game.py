604c4c2c5c5e18a4e95c7b39d1eea1609711894f
Arush Agrawal <arushagrawal366@gmail.com>
2026-07-03 15:06:40 +0530

    coded boilerplater stuff

--- chess_game.py
+++ chess_game.py
@@ -2,7 +2,7 @@ import chess
 import torch
 from torch import nn
 import numpy as np
-
+import chess.pgn
 #function to convert chess board to tensor
 def board_to_tensor(board):
     #mapping
@@ -30,9 +30,36 @@ def board_to_tensor(board):
 
 #encoding moves, giving them a unique number for model to understand later
 def encoding(move):
-    #formula we use is (start *63)+end
+    #formula we use is (start *64)+end
     return (move.from_square * 64)+ move.to_square
 
-chess_board= chess.Board()
-tensor_chess_board= board_to_tensor(chess_board)
-print(tensor_chess_board)
+# chess_board= chess.Board()
+# tensor_chess_board= board_to_tensor(chess_board)
+# print(tensor_chess_board)
+
+#writing boiler plate stuff
+class ChessModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.convstack= nn.Sequential(
+            nn.Conv2d(12,64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),#comprises the input to a smaller size 
+            nn.ReLU(),
+            nn.Conv2d(64,128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.Conv2d(128,128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.Conv2d(128,128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU()
+        )
+        self.classifier= nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(128*8*8, 4096),
+            nn.ReLU()
+        )
+    def forward(self, x):
+        return self.classifier(self.convstack(x))
+    
